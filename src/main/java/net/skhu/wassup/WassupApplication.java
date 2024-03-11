3e9040723cb2d076a840e0cdd010feb5fe1d5874
dd-jiyun <dlwldbs8015@gmail.com>
2024-03-11 21:45:08 +0900

    [#1] feat: 프로젝트 생성

---
+++ src/main/java/net/skhu/wassup/WassupApplication.java
@@ -0,0 +1,13 @@
+package net.skhu.wassup;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WassupApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WassupApplication.class, args);
+    }
+
+}
---
+++ src/test/java/net/skhu/wassup/WassupApplicationTests.java
@@ -0,0 +1,13 @@
+package net.skhu.wassup;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WassupApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
