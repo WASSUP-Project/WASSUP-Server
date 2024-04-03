aa718cba1c9e24713f92306514ab43a76beb2da2
dd-jiyun <dlwldbs8015@gmail.com>
2024-04-03 17:41:55 +0900

    [#14] feat: Swagger 관련 어노테이션 설정
    - 문서화를 할 때 설명을 추가할 수 있는 어노테이션을 설정하였습니다.

--- src/main/java/net/skhu/wassup/app/admin/api/dto/RequestSignup.java
+++ src/main/java/net/skhu/wassup/app/admin/api/dto/RequestSignup.java
@@ -1,22 +1,27 @@
 package net.skhu.wassup.app.admin.api.dto;
 
+import io.swagger.v3.oas.annotations.media.Schema;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.Pattern;
 import jakarta.validation.constraints.Size;
 
+@Schema(description = "회원가입 요청")
 public record RequestSignup(
-
+        @Schema(description = "아이디를 5자 이상 20자 이하로 입력해주세요.")
         @NotBlank(message = "아이디를 입력해주세요.")
         @Size(min = 5, max = 20, message = "아이디는 5자 이상 20자 이하로 입력해주세요.")
         String adminId,
 
+        @Schema(description = "비밀번호를 8자 이상 20자 이하로 입력해주세요.")
         @NotBlank(message = "비밀번호를 입력해주세요.")
         @Size(min = 8, max = 20, message = "비밀번호는 8자 이상 20자 이하로 입력해주세요.")
         String password,
 
+        @Schema(description = "이름")
         @NotBlank(message = "이름을 입력해주세요.")
         String name,
 
+        @Schema(description = "전화번호", example = "01012345678")
         @NotBlank(message = "전화번호를 입력해주세요.")
         @Pattern(regexp = "010[0-9]{8}", message = "전화번호는 형식에 맞게 11자리 숫자로 입력해주세요.")
         String phoneNumber) {
--- src/main/java/net/skhu/wassup/app/admin/api/dto/RequestVerify.java
+++ src/main/java/net/skhu/wassup/app/admin/api/dto/RequestVerify.java
@@ -1,15 +1,19 @@
 package net.skhu.wassup.app.admin.api.dto;
 
+import io.swagger.v3.oas.annotations.media.Schema;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.Pattern;
 import jakarta.validation.constraints.Size;
 
+@Schema(description = "인증 요청")
 public record RequestVerify(
 
+        @Schema(description = "전화번호", example = "01012345678")
         @NotBlank(message = "전화번호를 입력해주세요.")
         @Pattern(regexp = "010[0-9]{8}")
         String phoneNumber,
 
+        @Schema(description = "인증 코드")
         @NotBlank(message = "인증 코드를 입력해주세요.")
         @Size(min = 6, max = 6, message = "인증 번호는 6자리로 입력해주세요.")
         String inputCertificationCode) {
