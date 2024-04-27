e47556ea2f9b767d0f9817c9ce16d6e6a04b945b
dd-jiyun <dlwldbs8015@gmail.com>
2024-04-27 21:23:28 +0900

    [#39] feat: DB 더미데이터 추가

---
+++ src/main/resources/database/data.sql
@@ -0,0 +1,7 @@
+INSERT INTO admin_table (create_date, update_date, admin_account_id, name, password, phone_number)
+values ('2024-04-01 00:00:00', '2024-04-01 00:00:00', 'admin', 'admin', 'admin', 'admin');
+
+INSERT INTO group_table (create_date, group_admin_id, update_date, address, business_number, description, email,
+                         group_unique_code, image_url, name)
+values ('2024-04-01 00:00:00', '1', '2024-04-01 00:00:00', '서울시 강남구', '123-45-67890', '테스트 그룹입니다.', '123@gmail.com',
+        '12A3B4', 'groupDefault.png', '테스트그룹');
