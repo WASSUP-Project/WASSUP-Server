b684d5465fb2bf994ca68e192262a9030a0d1a5e
dd-jiyun <dlwldbs8015@gmail.com>
2024-05-08 13:21:15 +0900

    [#36] feat: 출석 관련 DTO 추가
    - 출석을 위해 링크 고유 코드와 휴대폰 번호 끝 4자리를 입력받습니다.
    - 입력받은 끝 4자리의 휴대폰 번호와 일치하는 멤버를 찾아 반환합니다.

---
+++ src/main/java/net/skhu/wassup/app/attendance/api/dto/ResponseAttendanceMember.java
@@ -0,0 +1,17 @@
+package net.skhu.wassup.app.attendance.api.dto;
+
+import io.swagger.v3.oas.annotations.media.Schema;
+import lombok.Builder;
+
+@Builder
+@Schema(description = "출석 멤버 조회")
+public record ResponseAttendanceMember(
+
+        @Schema(description = "멤버 ID")
+        Long memberId,
+
+        @Schema(description = "멤버 이름")
+        String memberName
+) {
+
+}
