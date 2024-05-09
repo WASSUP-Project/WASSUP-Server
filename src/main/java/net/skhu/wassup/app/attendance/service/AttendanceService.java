75db7f0ac16492213400998582ca333bf4526b1b
dd-jiyun <dlwldbs8015@gmail.com>
2024-05-09 13:24:28 +0900

    [#36] feat: 출석 관련 로직 추상화

--- src/main/java/net/skhu/wassup/app/attendance/service/AttendanceService.java
+++ src/main/java/net/skhu/wassup/app/attendance/service/AttendanceService.java
@@ -1,11 +1,16 @@
 package net.skhu.wassup.app.attendance.service;
 
+import java.util.List;
+import net.skhu.wassup.app.attendance.api.dto.RequestAttendance;
+import net.skhu.wassup.app.attendance.api.dto.ResponseAttendanceMember;
 import net.skhu.wassup.app.attendance.api.dto.ResponseCode;
 
 public interface AttendanceService {
 
     ResponseCode generateAttendanceCode(Long groupId);
 
-    Long test(String code);
+    List<ResponseAttendanceMember> findMembers(RequestAttendance requestAttendance);
+
+    void saveAttendance(Long memberId);
 
 }
