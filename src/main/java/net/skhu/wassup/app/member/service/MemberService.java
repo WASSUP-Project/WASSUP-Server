03eed9cf6193ecf711b8a40ef210d226532cd1ff
dd-jiyun <dlwldbs8015@gmail.com>
2024-05-10 22:33:10 +0900

    [#49] feat: 멤버 수정 기능 추상화

--- src/main/java/net/skhu/wassup/app/member/service/MemberService.java
+++ src/main/java/net/skhu/wassup/app/member/service/MemberService.java
@@ -1,6 +1,7 @@
 package net.skhu.wassup.app.member.service;
 
 import net.skhu.wassup.app.member.api.dto.RequestMember;
+import net.skhu.wassup.app.member.api.dto.RequestUpdateMember;
 import net.skhu.wassup.app.member.api.dto.ResponseMember;
 
 public interface MemberService {
@@ -8,5 +9,7 @@ public interface MemberService {
     void saveMember(RequestMember requestMember);
 
     ResponseMember getMember(Long id);
+
+    void updateMember(Long id, RequestUpdateMember requestUpdateMember);
 
 }
