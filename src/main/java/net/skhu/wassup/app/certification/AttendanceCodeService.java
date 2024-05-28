16027182f5660bd60a506db586cbfdeee55eb633
dd-jiyun <dlwldbs8015@gmail.com>
2024-05-28 23:47:40 +0900

    [#66] feat: 출석 링크 번호 만료 시간 설정
    - 출석 링크를 생성하고 당일 24시까지만 코드가 유효합니다.

--- src/main/java/net/skhu/wassup/app/certification/AttendanceCodeService.java
+++ src/main/java/net/skhu/wassup/app/certification/AttendanceCodeService.java
@@ -1,20 +1,35 @@
 package net.skhu.wassup.app.certification;
 
 import java.security.SecureRandom;
-import java.util.HashMap;
-import java.util.Map;
+import java.util.concurrent.TimeUnit;
 import lombok.extern.slf4j.Slf4j;
+import net.jodah.expiringmap.ExpirationPolicy;
+import net.jodah.expiringmap.ExpiringMap;
 import org.springframework.stereotype.Service;
 
 @Slf4j
 @Service
 public class AttendanceCodeService {
 
-    private final Map<String, Long> attendanceCode = new HashMap<>();
+    private final ExpiringMap<String, Long> attendanceCode = ExpiringMap.builder()
+            .variableExpiration()
+            .expirationPolicy(ExpirationPolicy.CREATED)
+            .expirationListener((key, value) -> log.info("출석 코드 만료 : {}", key))
+            .build();
+
+    private long getExpirationTime() {
+        long currentTimeMillis = System.currentTimeMillis();
+        long midnightMillis = currentTimeMillis + (24 * 60 * 60 * 1000)
+                - (currentTimeMillis % (24 * 60 * 60 * 1000));
+
+        return midnightMillis - currentTimeMillis;
+    }
 
     public String createCode(Long groupId) {
         String code = generateUniqueCode();
-        attendanceCode.put(code, groupId);
+        long expirationTime = getExpirationTime();
+        attendanceCode.put(code, groupId, expirationTime, TimeUnit.MILLISECONDS);
+
         return code;
     }
 
