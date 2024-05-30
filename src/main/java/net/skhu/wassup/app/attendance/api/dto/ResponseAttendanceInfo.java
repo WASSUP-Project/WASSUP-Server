6e0b92a580dafd6500bdb75a21df49fe18d420a9
dd-jiyun <dlwldbs8015@gmail.com>
2024-05-30 16:33:21 +0900

    [#72] fix: 하원 멤버 관련 기능 수정
    - 이미 하원한 멤버는 하원 멤버 리스트 조회 시 제외됩니다.
    - 금일 출석률을 조회합니다.

--- src/main/java/net/skhu/wassup/app/attendance/api/dto/ResponseAttendanceInfo.java
+++ src/main/java/net/skhu/wassup/app/attendance/api/dto/ResponseAttendanceInfo.java
@@ -1,10 +1,17 @@
 package net.skhu.wassup.app.attendance.api.dto;
 
+import io.swagger.v3.oas.annotations.media.Schema;
 import java.util.List;
 import lombok.Builder;
 
 @Builder
+@Schema(description = "출석 정보 조회")
 public record ResponseAttendanceInfo(
+
+        @Schema(description = "출석률")
         int attendanceRate,
+
+        @Schema(description = "미출석 멤버 조회")
         List<ResponseAttendanceMember> notAttendanceMembers) {
+
 }
