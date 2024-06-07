ccd3859ed9f2e0ea7007c56f4d22e391022da352
dd-jiyun <dlwldbs8015@gmail.com>
2024-06-07 20:42:31 +0900

    [#83] feat: 멤버 개별 공지사항 전송 기능 구현
    - 한 멤버에게 개별적으로 공지사항을 전송할 수 있습니다.

--- src/main/java/net/skhu/wassup/app/announcement/service/AnnouncementServiceImpl.java
+++ src/main/java/net/skhu/wassup/app/announcement/service/AnnouncementServiceImpl.java
@@ -2,6 +2,7 @@ package net.skhu.wassup.app.announcement.service;
 
 import static net.skhu.wassup.app.member.domain.JoinStatus.ACCEPTED;
 import static net.skhu.wassup.global.error.ErrorCode.NOT_FOUND_GROUP;
+import static net.skhu.wassup.global.error.ErrorCode.NOT_FOUND_MEMBER;
 import static net.skhu.wassup.global.error.ErrorCode.UNAUTHORIZED_ADMIN;
 
 import java.util.List;
@@ -14,6 +15,7 @@ import net.skhu.wassup.app.announcement.domain.AnnouncementRepository;
 import net.skhu.wassup.app.group.domain.Group;
 import net.skhu.wassup.app.group.domain.GroupRepository;
 import net.skhu.wassup.app.member.domain.Member;
+import net.skhu.wassup.app.member.domain.MemberRepository;
 import net.skhu.wassup.global.error.exception.CustomException;
 import net.skhu.wassup.global.message.SMSMessageSender;
 import org.springframework.stereotype.Service;
@@ -28,7 +30,19 @@ public class AnnouncementServiceImpl implements AnnouncementService {
 
     private final AnnouncementRepository announcementRepository;
 
+    private final MemberRepository memberRepository;
+
     private final SMSMessageSender smsMessageSender;
+
+    private boolean isCheckSendStatus(String phoneNumber, String title, String content) {
+        try {
+            smsMessageSender.send(phoneNumber, title, content);
+            return true;
+        } catch (Exception e) {
+            log.error(e.getMessage());
+            return false;
+        }
+    }
 
     private boolean isMemberFailedToReceiveAnnouncement(Member member, RequestAnnouncement requestAnnouncement) {
         return member.getJoinStatus().equals(ACCEPTED) && !isCheckSendStatus(member.getPhoneNumber(),
@@ -39,16 +53,6 @@ public class AnnouncementServiceImpl implements AnnouncementService {
         return group.getMembers().stream()
                 .filter(member -> isMemberFailedToReceiveAnnouncement(member, requestAnnouncement))
                 .toList();
-    }
-
-    private boolean isCheckSendStatus(String phoneNumber, String title, String content) {
-        try {
-            smsMessageSender.send(phoneNumber, title, content);
-            return true;
-        } catch (Exception e) {
-            log.error(e.getMessage());
-            return false;
-        }
     }
 
     private void saveAnnouncement(Group group, RequestAnnouncement requestAnnouncement) {
@@ -75,6 +79,37 @@ public class AnnouncementServiceImpl implements AnnouncementService {
         return failedMembers.stream()
                 .map(Member::toString)
                 .toList();
+    }
+
+    private boolean isNotGroupMember(Long memberId, Group group) {
+        return group.getMembers().stream()
+                .noneMatch(member -> member.getId().equals(memberId));
+    }
+
+    private String findPhoneNumber(Long memberId) {
+        return memberRepository.findById(memberId)
+                .orElseThrow(() -> new CustomException(NOT_FOUND_MEMBER))
+                .getPhoneNumber();
+    }
+
+    @Override
+    @Transactional
+    public void writeAnnouncementToMember(Long id, Long groupId, Long memberId,
+                                          RequestAnnouncement requestAnnouncement) {
+        Group group = getGroupOrThrow(groupId);
+
+        if (isNotUserGroupAdmin(id, group)) {
+            throw new CustomException(UNAUTHORIZED_ADMIN);
+        }
+
+        if (isNotGroupMember(memberId, group)) {
+            throw new CustomException(NOT_FOUND_MEMBER);
+        }
+
+        String messageTitle = String.format("[%s] %s", group.getName(), requestAnnouncement.title());
+        String phoneNumber = findPhoneNumber(memberId);
+
+        isCheckSendStatus(phoneNumber, messageTitle, requestAnnouncement.content());
     }
 
     private ResponseAnnouncement mapToResponseAnnouncement(Announcement announcement) {
