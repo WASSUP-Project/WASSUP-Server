e71422ec94dab363ce27d93534c9e5a4275e2d9a
dd-jiyun <dlwldbs8015@gmail.com>
2024-06-08 22:55:40 +0900

    [#85] feat: 관리자 정보 조회 기능 구현
    - 관리자의 정보를 조회하여 자신이 관리하고 있는 그룹과 멤버의 수를 확인할 수 있습니다.

--- src/main/java/net/skhu/wassup/app/admin/api/dto/ResponseAdmin.java
+++ src/main/java/net/skhu/wassup/app/admin/api/dto/ResponseAdmin.java
@@ -1,6 +1,7 @@
 package net.skhu.wassup.app.admin.api.dto;
 
 import io.swagger.v3.oas.annotations.media.Schema;
+import java.time.LocalDateTime;
 import lombok.Builder;
 
 @Builder
@@ -14,6 +15,27 @@ public record ResponseAdmin(
         String name,
 
         @Schema(description = "전화번호")
-        String phoneNumber) {
+        String phoneNumber,
+
+        @Schema(description = "생성일")
+        String createdAt,
+
+        @Schema(description = "그룹 수")
+        int groupCount,
+
+        @Schema(description = "멤버 수")
+        int memberCount) {
+
+    public static ResponseAdmin of(Long id, String name, String phoneNumber, LocalDateTime createdAt, int groupCount,
+                                   int memberCount) {
+        return ResponseAdmin.builder()
+                .id(id)
+                .name(name)
+                .phoneNumber(phoneNumber)
+                .createdAt(createdAt.toLocalDate().toString())
+                .groupCount(groupCount)
+                .memberCount(memberCount)
+                .build();
+    }
 
 }
