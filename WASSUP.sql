adc10876857fe2fb6b8754996d943e29427e6f5f
dd-jiyun <dlwldbs8015@gmail.com>
2024-06-10 11:11:28 +0900

    [#88] feat: 멤버 더미 데이터 추가

--- WASSUP.sql
+++ WASSUP.sql
@@ -1,24 +1,24 @@
 ﻿-- admin 테이블
 CREATE TABLE `admin`
 (
-    `id`           INT AUTO_INCREMENT PRIMARY KEY COMMENT 'Auto Increment',
-    `admin_id`     VARCHAR(255) NOT NULL COMMENT '관리자 ID',
-    `password`     VARCHAR(255) NOT NULL,
-    `name`         VARCHAR(255) NOT NULL,
-    `phone_number` VARCHAR(255) NOT NULL
+    `id`               INT AUTO_INCREMENT PRIMARY KEY COMMENT 'Auto Increment',
+    `admin_account_id` VARCHAR(255) NOT NULL COMMENT '관리자 ID',
+    `password`         VARCHAR(255) NOT NULL,
+    `name`             VARCHAR(255) NOT NULL,
+    `phone_number`     VARCHAR(255) NOT NULL
 );
 
 -- group 테이블
 CREATE TABLE `group`
 (
-    `id`                     INT AUTO_INCREMENT PRIMARY KEY COMMENT 'Auto Increment',
-    `name`                   VARCHAR(255) NOT NULL,
-    `representative_name`    VARCHAR(255) NOT NULL,
-    `representative_email`   VARCHAR(255) NOT NULL,
-    `business_number`        VARCHAR(255) NULL COMMENT '사업자 번호는 선택 (String!! -가 포함됨)',
-    `representative_address` VARCHAR(255) NULL,
-    `introduction`           VARCHAR(255) NOT NULL,
-    `user_id`                INT          NOT NULL COMMENT '그룹에 해당하는 사용자 ID'
+    `id`              INT AUTO_INCREMENT PRIMARY KEY COMMENT 'Auto Increment',
+    `name`            VARCHAR(255) NOT NULL,
+    `description`     VARCHAR(255) NOT NULL,
+    `address`         VARCHAR(255),
+    `business_number` VARCHAR(255) COMMENT '사업자 번호는 선택',
+    `email`           VARCHAR(255) NOT NULL,
+    `image_url`       VARCHAR(255),
+    `user_id`         INT          NOT NULL COMMENT '그룹에 해당하는 유저 ID'
 );
 
 -- announcement 테이블
@@ -26,36 +26,38 @@ CREATE TABLE `announcement`
 (
     `id`       INT AUTO_INCREMENT PRIMARY KEY COMMENT 'Auto Increment',
     `title`    VARCHAR(255) NOT NULL,
-    `content`  VARCHAR(255) NOT NULL,
-    `group_id` INT          NOT NULL COMMENT '그룹의 ID로 여러 공지사항을 관리 가능'
+    `content`  TEXT         NOT NULL,
+    `group_id` INT          NOT NULL
 );
 
 -- member 테이블
 CREATE TABLE `member`
 (
     `id`              INT AUTO_INCREMENT PRIMARY KEY COMMENT 'Auto Increment',
-    `name`            VARCHAR(255) NOT NULL COMMENT '원생 이름',
-    `phone_number`    VARCHAR(255) NOT NULL COMMENT '번호 뒤 4자리를 잘라서 대조하는 방식 사용',
-    `is_matching`     BOOLEAN      NOT NULL DEFAULT FALSE COMMENT '뒷자리 & 얼굴인식 일치 여부',
-    `status`          ENUM('WAITING', 'COMPLETED', 'CANCELLED') NOT NULL DEFAULT 'WAITING' COMMENT '가입 상태 (대기중, 완료, 취소)',
-    `announcement_id` INT          NOT NULL COMMENT '공지사항 ID',
-    `group_id`        INT          NOT NULL COMMENT '속한 그룹의 ID'
+    `group_id`        INT                          NOT NULL COMMENT '속한 그룹의 ID',
+    `name`            VARCHAR(255)                 NOT NULL COMMENT '원생 이름',
+    `phone_number`    VARCHAR(255)                 NOT NULL COMMENT '학부모 번호',
+    `birth`           DATE                         NOT NULL COMMENT '생년월일',
+    `field`           VARCHAR(255) COMMENT '특이사항',
+    `status`          ENUM ('WAITING', 'ACCEPTED') NOT NULL DEFAULT 'WAITING' COMMENT '가입 상태 (대기중, 완료)',
+    `announcement_id` INT                          NOT NULL COMMENT '공지사항 ID'
 );
 
 -- attendance 테이블
 CREATE TABLE `attendance`
 (
     `id`          INT AUTO_INCREMENT PRIMARY KEY COMMENT 'Auto Increment',
-    `status`      ENUM('ATTENDANCE', 'ABSENCE', 'SICK_LEAVE', 'OTHER') NOT NULL COMMENT '출석, 결석, 병결, 기타',
-    `created_at`  DATETIME NOT NULL COMMENT '생성 시간',
-    `modified_at` DATETIME NOT NULL COMMENT '수정 시간',
-    `member_id`   INT      NOT NULL
+    `status`      ENUM ('ATTENDANCE', 'ABSENCE', 'SICK_LEAVE', 'OTHER') NOT NULL COMMENT '출석, 결석, 병결, 기타',
+    `created_at`  DATETIME                                              NOT NULL COMMENT '생성 시간',
+    `modified_at` DATETIME                                              NOT NULL COMMENT '수정 시간',
+    `member_id`   INT                                                   NOT NULL,
+    `group_id`    INT                                                   NOT NULL
 );
 
--- sent_history 테이블
-CREATE TABLE `sent_history`
+-- open_days 테이블
+CREATE TABLE `open_days`
 (
-    `id`              INT AUTO_INCREMENT PRIMARY KEY COMMENT 'Auto Increment',
-    `announcement_id` INT NOT NULL,
-    `member_id`       INT NOT NULL
+    `id`          INT AUTO_INCREMENT PRIMARY KEY COMMENT 'Auto Increment',
+    `create_date` DATE NOT NULL COMMENT '생성 날짜',
+    `group_id`    INT  NOT NULL COMMENT '그룹 ID'
 );
--- src/main/resources/database/data.sql
+++ src/main/resources/database/data.sql
@@ -7,3 +7,23 @@ INSERT INTO group_table (create_date, group_admin_id, update_date, address, busi
 values ('2024-04-01 00:00:00', '1', '2024-04-01 00:00:00', '경기 성남시 분당구 판교역로 166 (백현동, 카카오 판교 아지트) 2층', '123-45-67890',
         '테스트 그룹입니다.', '123@gmail.com',
         '12A3B4', 'groupDefault.png', '테스트그룹');
+
+INSERT INTO member_table (join_status, create_date, group_id, member_id, update_date, birth, name, phone_number,
+                          specifics)
+values ('1', '2024-06-01 00:00:00', '1', '1', '2024-06-01 00:00:00', '2008-06-01', '테스트멤버1', '01012341234',
+        '테스트 멤버 입니다.');
+
+INSERT INTO member_table (join_status, create_date, group_id, member_id, update_date, birth, name, phone_number,
+                          specifics)
+values ('1', '2024-06-01 00:00:00', '1', '2', '2024-06-01 00:00:00', '2008-06-01', '테스트멤버2', '01012341234',
+        '테스트 멤버 입니다.');
+
+INSERT INTO member_table (join_status, create_date, group_id, member_id, update_date, birth, name, phone_number,
+                          specifics)
+values ('1', '2024-06-01 00:00:00', '1', '3', '2024-06-01 00:00:00', '2008-06-01', '테스트멤버3', '01043214321',
+        '테스트 멤버 입니다.');
+
+INSERT INTO member_table (join_status, create_date, group_id, member_id, update_date, birth, name, phone_number,
+                          specifics)
+values ('1', '2024-06-01 00:00:00', '1', '4', '2024-06-01 00:00:00', '2008-06-01', '테스트멤버4', '01043214321',
+        '테스트 멤버 입니다.');
